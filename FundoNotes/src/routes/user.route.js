1f3db80afa42e83515acaea5612361942f5a052f
AbhiShinde1 <abhishinde572@gmail.com>
2022-11-17 11:46:02 +0530

    [Added : NewUserRegistration features Using CRUD_Operation]

--- FundoNotes/src/routes/user.route.js
+++ FundoNotes/src/routes/user.route.js
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import express from 'express';
 import * as userController from '../controllers/user.controller';
 import { newUserValidator } from '../validators/user.validator';
@@ -6,5 +7,17 @@ const router = express.Router();
 
 //routes to register a new user
 router.post('/Register', newUserValidator, userController.RegisterNewUser);
+
+//route to get all users
+router.get('', userController.getAllUsers);
+
+//route to get a single user by their user id
+router.get('/:_id', userController.getUser);
+
+//route to update a single user by their user id
+router.put('/:_id', userController.updateUser);
+
+//route to delete a single user by their user id
+router.delete('/:_id', userController.deleteUser);
 
 export default router;
--- FundoNotes/src/services/user.service.js
+++ FundoNotes/src/services/user.service.js
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import User from '../models/user.model';
 import bcrypt from 'bcrypt';
 
@@ -19,4 +20,36 @@ export const Registration = async (body) => {
     const data = await User.create(body);
     return data;
   }
+};
+
+//get all users
+export const getAllUsers = async () => {
+  const data = await User.find();
+  return data;
+};
+
+//update single user
+export const updateUser = async (_id, body) => {
+  const data = await User.findByIdAndUpdate(
+    {
+      _id
+    },
+    body,
+    {
+      new: true
+    }
+  );
+  return data;
+};
+
+//delete single user
+export const deleteUser = async (id) => {
+  await User.findByIdAndDelete(id);
+  return '';
+};
+
+//get single user
+export const getUser = async (_id) => {
+  const data = await User.findById(_id);
+  return data;
 };
