eb9273c58a14cdaae310a53f82a1f5ef23774dc2
AbhiShinde1 <abhishinde572@gmail.com>
2022-11-23 11:06:08 +0530

    [Added: Test case Scenarios of UserRegistration_AND_Login User]

--- FundoNotes/tests/integration/user.test.js
+++ FundoNotes/tests/integration/user.test.js
@@ -1,3 +1,6 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable quotes */
+/* eslint-disable prettier/prettier */
 import { expect } from 'chai';
 import request from 'supertest';
 import mongoose from 'mongoose';
@@ -26,16 +29,157 @@ describe('User APIs Test', () => {
     done();
   });
 
-  describe('GET /users', () => {
-    it('should return empty array', (done) => {
-      request(app)
-        .get('/api/v1/users')
-        .end((err, res) => {
-          expect(res.statusCode).to.be.equal(200);
-          expect(res.body.data).to.be.an('array');
+  //1.Test case for user registration
 
+  describe('Userregistration', () => {
+    const inputBody={
+      "Firstname":"Abhishek",
+     "Lastname":"Shinde",
+      "EmailId":"abhishinde572@gmail.com",
+      "password":"Shinde124"
+    }
+    it('details of users should be saved in database', (done) => {
+      request(app)
+        .post('/api/v1/users/Register')
+        .send(inputBody)
+        .end((err, res) => {
+          expect(res.statusCode).to.be.equal(201);
           done();
         });
     });
   });
+
+//2.Test case for Invalid firstname
+
+describe('Userregistration =========> Invalid firstname', () => {
+  const inputBody={
+    "Firstname":"A",
+   "Lastname":"Shinde",
+    "EmailId":"abhishinde572@gmail.com",
+    "password":"Shinde124"
+  }
+  it('details of users should be saved in database', (done) => {
+    request(app)
+      .post('/api/v1/users/Register')
+      .send(inputBody)
+      .end((err, res) => {
+        expect(res.statusCode).to.be.equal(500);
+        done();
+      });
+  });
+});
+
+
+//3.Test case for Invalid Lastname
+
+describe('Userregistration=========> Invalid Lastname', () => {
+  const inputBody={
+    "Firstname":"Abhishek",
+   "Lastname":"S",
+    "EmailId":"abhishinde572@gmail.com",
+    "password":"Shinde124"
+  }
+  it('details of users should be saved in database', (done) => {
+    request(app)
+      .post('/api/v1/users/Register')
+      .send(inputBody)
+      .end((_err, res) => {
+        expect(res.statusCode).to.be.equal(500);
+        done();
+      });
+  });
+});
+
+//4.Test case for Invalid password
+
+describe('Userregistration ============> Invalid password', () => {
+  const inputBody={
+    "Firstname":"Abhishek",
+   "Lastname":"Shinde",
+    "EmailId":"abhishinde572@gmail.com",
+    "password":"Shin"
+  }
+  it('details of users should be saved in database', (done) => {
+    request(app)
+      .post('/api/v1/users/Register')
+      .send(inputBody)
+      .end((_err, res) => {
+        expect(res.statusCode).to.be.equal(500);
+        done();
+      });
+  });
+});
+
+//5.Test case for valid user login
+
+
+describe('UserLogin', () => {
+  const inputBody={
+    "EmailId":"abhishinde572@gmail.com",
+      "password":"Shinde124"
+  }
+  it('user details should be saved in database', (done) => {
+    request(app)
+      .post('/api/v1/users/logins')
+      .send(inputBody)
+      .end((err, res) => {
+      expect(res.statusCode).to.be.equal(200);
+      done();
+    });
+   });
+});
+
+ //6.Test case for invalid EmailId
+
+ describe('UserLogin=======>  invalid EmailId', () => {
+  const inputBody={
+    "EmailId":"abhishsddfdinde572@gmail.com",
+    "password":"Shinde"
+  }
+  it('user details should be saved in database', (done) => {
+    request(app)
+      .post('/api/v1/users/logins')
+      .send(inputBody)
+      .end((err, res) => {
+      expect(res.statusCode).to.be.equal(500);
+      done();
+    });
+   });
+});
+
+//7.Test case for invalid password
+
+describe('UserLogin=====> invalid password', () => {
+  const inputBody={
+    "EmailId": "abhishinde572@gmail.com",
+    "password":"Shinde12477"
+  }
+  it('user details should be saved in database', (done) => {
+    request(app)
+      .post('/api/v1/users/logins')
+      .send(inputBody)
+      .end((err, res) => {
+      expect(res.statusCode).to.be.equal(500);
+      done();
+    });
+   });
+});
+
+ //8.TestCAse both invalid password and email
+
+ describe('UserLogin====> Invalid password and email', () => {
+  const inputBody={
+    "EmailId": "abhishindedff572@gmail.com",
+    "password":"Shinde12477"
+  }
+  it('user details should be saved in database', (done) => {
+    request(app)
+      .post('/api/v1/users/logins')
+      .send(inputBody)
+      .end((err, res) => {
+      expect(res.statusCode).to.be.equal(500);
+      done();
+    });
+   });
+});
 });
